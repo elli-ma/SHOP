157b0b9b50c99038529737f635ab6407b1255211
elli-ma <elli9225@gmail.com>
2023-05-26 15:26:12 +0800

    data

---
+++ data.js
@@ -0,0 +1,234 @@
+const DATA = {
+    "category1": {
+        title: "Small Size",
+        products: [
+            {
+                "url": "img/category1/category1-1.png",
+                "name": "СУМКА-1",
+                "date": "01.02.2023",
+                "price": "37 100 RUB"
+            },
+            {
+                "url": "img/category1/category1-2.png",
+                "name": "СУМКА-2",
+                "date": "02.04.2022",
+                "price": "36 100 RUB"
+            },
+            {
+                "url": "img/category1/category1-3.png",
+                "name": "СУМКА-3",
+                "date": "02.02.2022",
+                "price": "38 100 RUB"
+            },
+            {
+                "url": "img/category1/category1-4.png",
+                "name": "СУМКА-4",
+                "date": "02.03.2022",
+                "price": "40 000 RUB"
+            },
+            {
+                "url": "img/category1/category1-5.png",
+                "name": "СУМКА-5",
+                "date": "02.05.2022",
+                "price": "41 000 RUB"
+            },
+            {
+                "url": "img/category1/category1-6.png",
+                "name": "СУМКА-6",
+                "date": "02.07.2022",
+                "price": "42 500 RUB"
+            },
+            {
+                "url": "img/category1/category1-7.png",
+                "name": "СУМКА-7",
+                "date": "02.08.2022",
+                "price": "43 000 RUB"
+            },
+            {
+                "url": "img/category1/category1-8.png",
+                "name": "СУМКА-8",
+                "date": "02.04.2022",
+                "price": "45 000 RUB"
+            },
+            {
+                "url": "img/category1/category1-9.png",
+                "name": "СУМКА-9",
+                "date": "02.03.2022",
+                "price": "46 000 RUB"
+            },
+            {
+                "url": "img/category1/category1-10.png",
+                "name": "СУМКА-10",
+                "date": "02.01.2022",
+                "price": "36 100 RUB"
+            },
+            {
+                "url": "img/category1/category1-11.png",
+                "name": "СУМКА-11",
+                "date": "02.02.2022",
+                "price": "38 100 RUB"
+            },
+            {
+                "url": "img/category1/category1-12.png",
+                "name": "СУМКА-12",
+                "date": "02.10.2022",
+                "price": "39 100 RUB"
+            }           
+        ]
+    },
+    "category2": {
+        title: "Medium Size",
+        products: [
+            {
+                "url": "img/category2/category2-1.png",
+                "name": "СУМКА-1",
+                "date": "01.02.2023",
+                "price": "37 100 RUB"
+            },
+            {
+                "url": "img/category2/category2-2.png",
+                "name": "СУМКА-2",
+                "date": "02.04.2022",
+                "price": "40 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-3.png",
+                "name": "СУМКА-3",
+                "date": "02.02.2022",
+                "price": "41 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-4.png",
+                "name": "СУМКА-4",
+                "date": "02.03.2022",
+                "price": "42 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-5.png",
+                "name": "СУМКА-5",
+                "date": "02.05.2022",
+                "price": "37 100 RUB"
+            },
+            {
+                "url": "img/category2/category2-6.png",
+                "name": "СУМКА-6",
+                "date": "02.07.2022",
+                "price": "36 500 RUB"
+            },
+            {
+                "url": "img/category2/category2-7.png",
+                "name": "СУМКА-7",
+                "date": "02.08.2022",
+                "price": "35 300 RUB"
+            },
+            {
+                "url": "img/category2/category2-8.png",
+                "name": "СУМКА-8",
+                "date": "02.04.2022",
+                "price": "48 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-9.png",
+                "name": "СУМКА-9",
+                "date": "02.03.2022",
+                "price": "47 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-10.png",
+                "name": "СУМКА-10",
+                "date": "02.01.2022",
+                "price": "50 000RUB"
+            },
+            {
+                "url": "img/category2/category2-11.png",
+                "name": "СУМКА-11",
+                "date": "02.02.2022",
+                "price": "36 100 RUB"
+            },
+            {
+                "url": "img/category2/category2-12.png",
+                "name": "СУМКА-12",
+                "date": "02.10.2022",
+                "price": "50 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-13.png",
+                "name": "СУМКА-13",
+                "date": "02.10.2022",
+                "price": "32 000 RUB"
+            },
+            {
+                "url": "img/category2/category2-14.png",
+                "name": "СУМКА-14",
+                "date": "02.10.2022",
+                "price": "40 000 RUB"
+            } 
+        ]
+    },
+    "category3": {
+        title: "Large size",
+        products: [
+            {
+                "url": "img/category3/category3-1.png",
+                "name": "СУМКА-1",
+                "date": "01.02.2023",
+                "price": "37 100 RUB"
+            },
+            {
+                "url": "img/category3/category3-2.png",
+                "name": "СУМКА-2",
+                "date": "02.04.2022",
+                "price": "40 000 RUB"
+            },
+            {
+                "url": "img/category3/category3-3.png",
+                "name": "СУМКА-3",
+                "date": "02.02.2022",
+                "price": "41 000 RUB"
+            },
+            {
+                "url": "img/category3/category3-4.png",
+                "name": "СУМКА-4",
+                "date": "02.03.2022",
+                "price": "42 000 RUB"
+            },
+            {
+                "url": "img/category3/category3-5.png",
+                "name": "СУМКА-5",
+                "date": "02.05.2022",
+                "price": "37 100 RUB"
+            },
+            {
+                "url": "img/category3/category3-6.png",
+                "name": "СУМКА-6",
+                "date": "02.07.2022",
+                "price": "36 500 RUB"
+            },
+            {
+                "url": "img/category3/category3-7.png",
+                "name": "СУМКА-7",
+                "date": "02.08.2022",
+                "price": "35 300 RUB"
+            },
+            {
+                "url": "img/category3/category3-8.png",
+                "name": "СУМКА-8",
+                "date": "02.04.2022",
+                "price": "48 000 RUB"
+            },
+            {
+                "url": "img/category3/category3-9.png",
+                "name": "СУМКА-9",
+                "date": "02.03.2022",
+                "price": "47 000 RUB"
+            },
+            {
+                "url": "img/category3/category3-10.png",
+                "name": "СУМКА-10",
+                "date": "02.01.2022",
+                "price": "50 000RUB"
+            }
+        ]
+    }
+
+}
--- script.js
+++ script.js
@@ -1,118 +1,111 @@
-// Получаем ссылку на кнопку "Купить"
-const buyButton = document.getElementById("Buy");
-
-// Получаем ссылку на блок формы
-const buyForm = document.getElementById("buyForm");
-
-// Получаем ссылки на кнопки внутри формы
-const submitButton = document.getElementById("submitBtn");
-const closeButton = document.getElementById("closeBtn");
-const Body = document.querySelector("body");
-
-// Обработчик клика на кнопку "Купить"
-buyButton.addEventListener("click", function () {
-  // Показываем блок формы
-  buyForm.style.display = "block";
-  Body.style.overflow = "hidden"
-});
-
-// Обработчик клика на кнопку "Купить" внутри формы
-submitButton.addEventListener("click", function () {
-  // Выполняем логику покупки товара
-  // ...
-
-  // Показываем сообщение о покупке (например, через alert)
-  alert("Товар успешно куплен!");
-
-  // Скрываем блок формы
-  buyForm.style.display = "none";
-  Body.style.overflow = "visible"
-});
-
-// Обработчик клика на кнопку "Закрыть" внутри формы
-closeButton.addEventListener("click", function () {
-  // Скрываем блок формы
-  buyForm.style.display = "none";
-  Body.style.overflow = "visible"
-});
+Date.prototype.getWeek = function () {
+  var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+};
 
 
-
-
-
-
-
-// Получаем ссылку на кнопку переключения темы
+// Переключения темы
 const themeToggle = document.getElementById("themeToggle");
-
-// Получаем ссылку на элемент body
 const wrapper = document.getElementById("wrapper");
 const menu = document.getElementById("menu");
 const link = document.querySelectorAll(".menu__link");
-
-
-// Переменная для отслеживания текущей темы
 let isDarkMode = false;
 
-// Обработчик клика на кнопку переключения темы
 themeToggle.addEventListener("click", function () {
-  // Переключаем флаг текущей темы
   isDarkMode = !isDarkMode;
-
-  // Применяем соответствующую тему
   if (isDarkMode) {
-    // Темная тема
     wrapper.style.backgroundColor = "#2b2b2b";
     wrapper.style.color = "white";
     menu.style.backgroundColor = "rgba(43, 43, 43, 0.95)";
-    themeToggle.style.backgroundColor = "white";
-    // menu.style.color = "white";
+    themeToggle.style.backgroundImage = "url(img/sun.svg)";
     link.forEach(function (link) {
-      // Здесь вы можете применить нужные стили к каждой ссылке при нажатии на кнопку
       link.style.color = "white";
-
     });
 
   } else {
-    // Светлая тема
     wrapper.style.backgroundColor = "white";
     wrapper.style.color = "black";
     menu.style.backgroundColor = "rgba(255, 255, 255, 0.95)";
     menu.style.color = "black";
-    themeToggle.style.backgroundColor = "#828282";
-
+    themeToggle.style.backgroundImage = "url(img/moon.svg)";
     link.forEach(function (link) {
-      // Здесь вы можете применить нужные стили к каждой ссылке при нажатии на кнопку
-      link.style.color = "black"; // Пример изменения цвета ссылки
+      link.style.color = "black";
     });
-    // Примените остальные стили для светлой темы
   }
 });
 
+// Формат даты
+function getDayInfo(dateString) {
+  const dateParts = dateString.split('.');
+  const day = parseInt(dateParts[0], 10);
+  const month = parseInt(dateParts[1], 10) - 1;
+  const year = parseInt(dateParts[2], 10);
+
+  const date = new Date(year, month, day);
+  const week = date.getWeek() % 4 + 1
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+
+  const formatter = new Intl.DateTimeFormat('ru-RU', options);
+  let [a, b] = formatter.format(date).split(',')
+  return [a, `, ${week} неделя`, b].join("")
+}
+
+// Вывод категорий и продуктов
+function productRender() {
+  let e = document.getElementById("categories")
+  let categories = DATA
+
+  for (categoryId in categories) {
+    let category = categories[categoryId]
+
+    const products = category.products.map(product => {
+      return `<article class="product-card">
+        <img src="${product.url}" alt="${product.name}" class="product-card__image">
+        <h3 class="product-card__title">${product.name}</h3>
+        <p class="product-card__date"><span class="product-card__date-value">${getDayInfo(product.date)}</span></p>
+        <div class="product-card_block">
+          <p class="product-card__price">${product.price}</p>
+          <button class="product-card__button" id="Buy">Купить</button>
+        </div>
+       </article>`
+    }).join("")
+
+    let res = `<section class="category" id="${categoryId}">
+      <h2 class="category__title">${category.title}</h2>
+      <div class="category__card">${products}</div>
+    </section>`
+
+    e.insertAdjacentHTML('beforeend', res);
+
+  }
+
+}
+productRender()
 
 
-
+// Кнопка вверх
 const btnUp = {
   el: document.querySelector('.btn-up'),
   show() {
-    // удалим у кнопки класс btn-up_hide
     this.el.classList.remove('btn-up_hide');
   },
   hide() {
-    // добавим к кнопке класс btn-up_hide
     this.el.classList.add('btn-up_hide');
   },
   addEventListener() {
-    // при прокрутке содержимого страницы
     window.addEventListener('scroll', () => {
-      // определяем величину прокрутки
       const scrollY = window.scrollY || document.documentElement.scrollTop;
-      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
       scrollY > 400 ? this.show() : this.hide();
     });
-    // при нажатии на кнопку .btn-up
     document.querySelector('.btn-up').onclick = () => {
-      // переместим в начало страницы
       window.scrollTo({
         top: 0,
         left: 0,
@@ -121,5 +114,27 @@ const btnUp = {
     }
   }
 }
-
 btnUp.addEventListener();
+
+// Кнопка "Купить"
+const buyButtons = document.querySelectorAll(".product-card__button");
+const buyForm = document.getElementById("buyForm");
+const submitButton = document.getElementById("submitBtn");
+const closeButton = document.getElementById("closeBtn");
+const Body = document.querySelector("body");
+
+buyButtons.forEach(buyButton => {
+  buyButton.addEventListener("click", function () {
+    buyForm.style.display = "block";
+    Body.style.overflow = "hidden"
+  });
+})
+submitButton.addEventListener("click", function () {
+  alert("Товар успешно куплен!");
+  buyForm.style.display = "none";
+  Body.style.overflow = "visible"
+});
+closeButton.addEventListener("click", function () {
+  buyForm.style.display = "none";
+  Body.style.overflow = "visible"
+});
